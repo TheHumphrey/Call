beeca58ba87c3b88440d96b0e810624749cd3f71
TheHumphrey <fernandob.delima@outlook.com>
2022-04-12 10:42:09 -0300

    update chat

--- src/pages/Room/Room.tsx
+++ src/pages/Room/Room.tsx
@@ -92,7 +92,6 @@ export const Room = () => {
         <TimerMenu>
           {`${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`}
         </TimerMenu>
-        <ResolutionMenu>HD</ResolutionMenu>
       </TopMenu>
 
       <BottomMenu>
